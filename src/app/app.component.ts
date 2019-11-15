e676811c23e889ce1c00517a9eb8581b33c3a016
Luc <luc.sergent@hotmail.fr>
2019-11-15 10:58:20 +0100

    dernier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) { }
 
   title = 'garageProjet';
+  goToAccueil(){
+    this.router.navigate(['/meca'])
+  }
+
 
 
 }
